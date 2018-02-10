8f6c5c3f665fc8ede7ec4fe7ec47240fcfdd8a19
Jason <Jason.Balladares001@mymdc.net>
2018-02-09 22:20:00 -0500

    added stuff

--- tutorApplication/app/src/main/java/com/example/jason/tutorapplication/LogInScreen.java
+++ tutorApplication/app/src/main/java/com/example/jason/tutorapplication/LogInScreen.java
@@ -1,5 +1,6 @@
 package com.example.jason.tutorapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -9,12 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogInScreen extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
-    //mPreferences will hold all the data
+    //mPreferences will hold user data
     //mEditor will be used to edit that data
     private SharedPreferences mPreferences;
     private SharedPreferences.Editor mEditor;
@@ -35,10 +35,11 @@ public class LogInScreen extends AppCompatActivity {
         btnLogin = findViewById(R.id.buttonLogin);
         mCheckBox = findViewById(R.id.checkbox);
 
-        //default settings for editing and storing of data
+        //make a shared preferences with the default file (strings.xml)
         mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         mEditor = mPreferences.edit();
 
+        //checks for for previous data
         checkSharedPreferences();
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +47,7 @@ public class LogInScreen extends AppCompatActivity {
             public void onClick(View view) {
                 //save the checkbox preference
                 if(mCheckBox.isChecked()){
+
                     //set a checkbox when the application starts
                     mEditor.putString(getString(R.string.checkbox), "True");
                     mEditor.commit();
@@ -63,19 +65,22 @@ public class LogInScreen extends AppCompatActivity {
                     Intent intent = new Intent(LogInScreen.this, MainMenu.class);
                     startActivity(intent);
 
-                }else{
-                    //set a checkbox when the application starts
-                    mEditor.putString(getString(R.string.checkbox), "False");
 
-                    mEditor.commit();
+                }else{
+                    buttonLogIn();
+
+                    //set a checkbox when the application starts
+                    //mEditor.putString(getString(R.string.checkbox), "False");
+
+                   // mEditor.commit();
 
                     //save the name
-                    mEditor.putString(getString(R.string.name), "");
-                    mEditor.commit();
+                   // mEditor.putString(getString(R.string.name), "");
+                   // mEditor.commit();
 
                     //save the password
-                    mEditor.putString(getString(R.string.password), "");
-                    mEditor.commit();
+                  //  mEditor.putString(getString(R.string.password), "");
+                  //  mEditor.commit();
                 }
             }
         });
@@ -83,9 +88,28 @@ public class LogInScreen extends AppCompatActivity {
 
     }
 
+    /**
+     * the user logs in without using the checkbox
+     */
+    public void buttonLogIn(){
+
+        mEditor.putString(getString(R.string.checkbox), "False");
+        //save the name
+        String name = mName.getText().toString();
+        mEditor.putString(getString(R.string.name), name);
+        mEditor.commit();
+
+        //save the password
+        String password = mPassword.getText().toString();
+        mEditor.putString(getString(R.string.password), password);
+        mEditor.commit();
 
 
+        Intent intent = new Intent(LogInScreen.this, MainMenu.class);
+        startActivity(intent);
 
+
+    }
 
     /**
      * Check the shared preferences and set them accordingly
@@ -96,17 +120,16 @@ public class LogInScreen extends AppCompatActivity {
         String password = mPreferences.getString(getString(R.string.password), "");
 
 
-
-        mName.setText(name);
-        mPassword.setText(password);
-
         if(checkbox.equals("True")){
             mCheckBox.setChecked(true);
+            mName.setText(name);
+            mPassword.setText(password);
         }else{
             mCheckBox.setChecked(false);
+            mName.setText("");
+            mPassword.setText("");
         }
 
     }
-
 
 }
--- tutorApplication/app/src/main/java/com/example/jason/tutorapplication/MainMenu.java
+++ tutorApplication/app/src/main/java/com/example/jason/tutorapplication/MainMenu.java
@@ -1,19 +1,31 @@
 package com.example.jason.tutorapplication;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
 
-    private static final String TAG = "SecondActivity";
-    private TextView mName;
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = mPreferences.edit();
+
+        Toast toast = Toast.makeText(MainMenu.this,"Logged In " + mPreferences.getString(getString(R.string.name), ""), Toast.LENGTH_SHORT);
+        toast.show();
+
 
     }
+
+
 }
