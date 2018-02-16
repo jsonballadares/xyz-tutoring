6f536f12477212967ba03cfedc8b1715b68be160
Jason <Jason.Balladares001@mymdc.net>
2018-02-15 20:33:45 -0500

    added stuff

--- tutorApplication/app/src/main/java/com/example/jason/tutorapplication/RegistrationActivity.java
+++ tutorApplication/app/src/main/java/com/example/jason/tutorapplication/RegistrationActivity.java
@@ -2,17 +2,32 @@ package com.example.jason.tutorapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etEmail;
+    private EditText etAddress;
+    private EditText etPhone;
+    private EditText etChildsName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        Toast toast = Toast.makeText(RegistrationActivity.this,"Welcome to XYZ!",Toast.LENGTH_SHORT);
-        toast.show();
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
+        etAddress = findViewById(R.id.etAddress);
+        etPhone = findViewById(R.id.etPhone);
+        etChildsName = findViewById(R.id.etChildsName);
+
+
 
     }
 }
